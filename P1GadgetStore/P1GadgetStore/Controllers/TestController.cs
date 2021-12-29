796ce6fd9902e7e31bc996c213a012f3574c270c
mikeo6000 <mikeo6000@hotmail.com>
2021-12-29 17:37:23 -0500

    P0

---
+++ P1GadgetStore/P1GadgetStore/Controllers/TestController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+namespace P1GadgetStore.Controllers
+{
+    public class TestController
+    {
+        private static readonly List<int> s_samples = new() { 12 };
+
+        // [Route] works for all http methods(verbs)
+        // [HttpGet], [HttpPost], etc - to also limit it to a specific method.
+
+        // the job of an action method is to handle one set of requests
+        // and return some "result" which asp.net will turn into the response
+        [HttpGet("/test")]
+        [HttpGet("/test")] // c# supports multiple attributes (same or different type)
+        public ContentResult GetSamples()
+        {
+            // asp.net provides a bunch of data types under the IActionResult interface
+            // and/or the ActionResult abstract base class.
+            // the job of an action result is to deserialize itself into an http response.
+            // a basic one we can start with is ContentResult.
+            // good for when you have something to put in the response body.
+            // (otherwise... maybe StatusCodeResult)
+
+            string json = JsonSerializer.Serialize(s_samples);
+
+            var result = new ContentResult()
+            {
+                StatusCode = 200,
+                ContentType = "application/json",
+                Content = json
+            };
+
+            return result;
+        }
+
+        // "model binding" runs before the action method:
+        //   - looks at the action method parameters, tries to fill them in
+        //     by deserializing parts of the request, based on the parameter name and type.
+        //   - [From___] attributes can influence/tell model binding what to do too
+        //   - if model binding doesn't find anything in the request... it'll be left at default (not exception)
+        //       so it's important to validate those parameters. (user input)
+        [HttpPost("/test")]
+        public ContentResult AddSample([FromBody] int sample)
+        {
+            // this is bad... List is NOT threadsafe. (use ConcurrentList iirc)
+            s_samples.Add(sample);
+            string json = JsonSerializer.Serialize(sample);
+
+            // good practice with POST, return a representation of the created resource in the body.
+            return new ContentResult()
+            {
+                StatusCode = StatusCodes.Status201Created,
+                ContentType = "application/json",
+                Content = json
+            };
+        }
+    }
+}
---
+++ P1GadgetStore/P1GadgetStore/Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+app.Map("/test", async context =>
+{
+    //await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes("Hello from map1"));
+    await context.Response.WriteAsync("Hello from map1");
+});
+app.Run();
+
